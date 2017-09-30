90ccc19ebdb82104adc86123353c658f51e8b323
wang935415150 <935415150@qq.com>
2017-09-30 21:48:59 +0800

    9.30-21-48

--- auto_server/api/plugins/memory.py
+++ auto_server/api/plugins/memory.py
@@ -1,39 +1,40 @@
 from repository import models
-class Disk(object):
+class Memory(object):
     def __init__(self,server_obj,info):
         self.server_obj = server_obj
-        self.disk_dict = info
+        self.memory_dict = info
         self.hostname=server_obj.hostname
     def process(self):
-        if not self.disk_dict['status'] :
-            errorlog=models.ErrorLog.objects.create(server_obj=self.server_obj,title='%s硬盘获取失败'%(self.hostname),content=self.disk_dict['msg'])
+        if not self.memory_dict['status'] :
+            errorlog=models.ErrorLog.objects.create(server_obj=self.server_obj,title='%s内存获取失败'%(self.hostname),content=self.memory_dict['msg'])
         else:
-            new_disk_info_dict = self.disk_dict['data']
-            new_disk_info_list = self.server_obj.disk.all()
-            new_disk_slot_set = set(new_disk_info_dict.keys())
-            old_disk_slot_set = {obj.slot for obj in new_disk_info_list}
-            add_slot_list = new_disk_slot_set.difference(old_disk_slot_set)
-            del_slot_list = old_disk_slot_set.difference(new_disk_slot_set)
-            update_slot_list = old_disk_slot_set.intersection(new_disk_slot_set)
+            new_memory_info_dict = self.memory_dict['data']
+            new_memory_info_list = self.server_obj.memory.all()
+            new_memory_slot_set = set(new_memory_info_dict.keys())
+            old_memory_slot_set = {obj.slot for obj in new_memory_info_list}
+            add_slot_list = new_memory_slot_set.difference(old_memory_slot_set)
+            del_slot_list = old_memory_slot_set.difference(new_memory_slot_set)
+            update_slot_list = old_memory_slot_set.intersection(new_memory_slot_set)
             add_record_list = []
             for slot in add_slot_list:
-                value = new_disk_info_dict[slot]
-                tmp = "[%s]添加硬盘[%s]"(self.server_obj.hostname,slot,)
+                value = new_memory_info_dict[slot]
+                tmp = "[%s]添加内存[%s]"(self.server_obj.hostname,slot,)
                 add_record_list.append(tmp)
                 value['server_obj'] = self.server_obj
                 models.Disk.objects.create(**value)
             models.ServerRecord.objects.create(server_obj=self.server_obj,content=';'.join(add_record_list))
-            delete_record_list = '[%s]删除了硬盘[%s]'%(self.server_obj.hostname,';'.join(del_slot_list))
+            delete_record_list = '[%s]删除了内存[%s]'%(self.server_obj.hostname,';'.join(del_slot_list))
             models.Disk.objects.filter(server_obj=self.server_obj, slot__in=del_slot_list).delete()
             models.ServerRecord.objects.create(server_obj=self.server_obj, content=';'.join(delete_record_list))
             update_record_list=[]
             for slot in update_slot_list:
-                value = new_disk_info_dict[slot]
+                value = new_memory_info_dict[
+                    slot]
                 obj = models.Disk.objects.filter(server_obj=self.server_obj, slot=slot).first()
                 for k, new_val in value.items():
                     old_val = getattr(obj, k)
                     if old_val != new_val:
-                        tmp='%s更新了硬盘%s的%s从%s更新到%s'%(self.hostname,slot,k,old_val,new_val)
+                        tmp='%s更新了内存%s的%s从%s更新到%s'%(self.hostname,slot,k,old_val,new_val)
                         update_record_list.append(tmp)
                         setattr(obj, k, new_val)
                 obj.save()
--- auto_server/api/plugins/nic.py
+++ auto_server/api/plugins/nic.py
@@ -10,31 +10,35 @@ class Nic(object):
             errorlog=models.ErrorLog.objects.create(server_obj=self.server_obj,title='%s网卡获取失败'%(self.hostname),content=self.nic_dict['msg'])
         else:
             new_nic_info_dict=self.nic_dict['data']
-            #{'status': True, 'data': {'eth0': {'up': True, 'hwaddr': '00:1c:42:a5:57:7a', 'ipaddrs': '10.211.55.4', 'netmask': '255.255.255.0'}}, 'msg': None}
             print(new_nic_info_dict)
             old_nic_info_dict=self.server_obj.nic.all()
             print(old_nic_info_dict)
-            new_nic_slot_set=set(new_nic_info_dict.keys())
-            old_nic_slot_set={obj.name for obj in new_nic_info_dict}
-            add_slot_list=new_nic_slot_set.difference(old_nic_slot_set)
-            del_slot_list=old_nic_slot_set.difference(new_nic_slot_set)
-            update_slot_list=old_nic_slot_set.intersection(new_nic_slot_set)
+            new_nic_name_set=set(new_nic_info_dict.keys())
+            old_nic_name_set={obj.name for obj in new_nic_info_dict}
+            add_name_list=new_nic_name_set.difference(old_nic_name_set)
+            del_name_list=old_nic_name_set.difference(new_nic_name_set)
+            update_name_list=old_nic_name_set.intersection(new_nic_name_set)
             add_record_list=[]
-            for eth in add_slot_list:
+            for eth in add_name_list:
                 value=new_nic_info_dict[eth]
-                tmp='添加网卡'
+                tmp="[%s]添加网卡[%s]"(self.server_obj.hostname,eth,)
                 add_record_list.append(tmp)
                 value['server_obj']=self.server_obj
                 models.NIC.objects.create(**value)
-
-            models.NIC.objects.filter(server_obj=self.server_obj,name__in=del_slot_list).delete()
-
-            for eth in update_slot_list:
+            models.ServerRecord.objects.create(server_obj=self.server_obj, content=';'.join(add_record_list))
+            delete_record_list = '[%s]删除了网卡[%s]' % (self.server_obj.hostname, ';'.join(del_name_list))
+            models.NIC.objects.filter(server_obj=self.server_obj,name__in=del_name_list).delete()
+            models.ServerRecord.objects.create(server_obj=self.server_obj, content=';'.join(delete_record_list))
+            update_record_list = []
+            for eth in update_name_list:
                 value=new_nic_info_dict[eth]
                 obj = models.NIC.objects.filter(server_obj=self.server_obj,name=eth).first()
                 for k,new_val in value.items():
                     old_val = getattr(obj,k)
                     if old_val!=new_val:
+                        tmp = '%s更新了硬盘%s的%s从%s更新到%s' % (self.hostname, eth, k, old_val, new_val)
+                        update_record_list.append(tmp)
                         setattr(obj,k,new_val)
                 obj.save()
+            models.ServerRecord.objects.create(server_obj=self.server_obj, content=';'.join(update_record_list))
 
